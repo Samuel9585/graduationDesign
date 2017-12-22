b1a5ca19e4d6893b9a90ee1028fdaa5d07f06c03
suzhijie <igeek_suzhijie@guoyanyunyan.local>
2017-12-22 13:54:58 +0800

    second page finished

--- dist/js/about.js
+++ dist/js/about.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// 进入页面时切换导航栏焦点
+// 进入页面时切换导航栏焦点,所有子页面都必须要有！！！这段代码
 var home = document.getElementsByClassName("nav_home")[0];
 var onPage = document.getElementsByClassName("nav_about")[0];
 var logo_bg = document.getElementById("log_bg");
@@ -11,3 +11,11 @@ function navBarchange(event) {
     logo_bg.style.backgroundColor = "#f39e38";
 }
 window.addEventListener("load", navBarchange, false);
+
+// 分割线
+var changeTarget = document.getElementsByClassName("content")[0];
+
+function addLoad() {
+    changeTarget.classList.add("load");
+}
+window.addEventListener("load", addLoad, false);
--- src/es6/about.js
+++ src/es6/about.js
@@ -1,4 +1,4 @@
-// 进入页面时切换导航栏焦点
+// 进入页面时切换导航栏焦点,所有子页面都必须要有！！！这段代码
 let home = document.getElementsByClassName("nav_home")[0];
 let onPage = document.getElementsByClassName("nav_about")[0];
 let logo_bg = document.getElementById("log_bg");
@@ -9,3 +9,11 @@ function navBarchange(event) {
     logo_bg.style.backgroundColor = "#f39e38";
 }
 window.addEventListener("load",navBarchange,false);
+
+// 分割线
+let changeTarget = document.getElementsByClassName("content")[0];
+
+function addLoad(){
+    changeTarget.classList.add("load");
+}
+window.addEventListener("load",addLoad,false);
