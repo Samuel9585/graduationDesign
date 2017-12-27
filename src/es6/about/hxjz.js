92921e34a416dce68070db38f7e760a7514412db
suzhijie <igeek_suzhijie@guoyanyunyan.local>
2017-12-27 20:44:45 +0800

    about/hxjz完成

---
+++ src/es6/about/hxjz.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// 进入页面时切换导航栏焦点,所有子页面都必须要有！！！这段代码
+var home = document.getElementsByClassName("nav_home")[0];
+var onPage = document.getElementsByClassName("nav_about")[0];
+var logo_bg = document.getElementById("log_bg");
+function navBarchange(event) {
+    var e = event || window.event;
+    home.classList.remove("active");
+    onPage.classList.add("active_about");
+    logo_bg.style.backgroundColor = "#f39e38";
+}
+window.addEventListener("load", navBarchange, false);
+
+// 分割线
