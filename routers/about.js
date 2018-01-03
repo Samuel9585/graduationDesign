5a47b1a62e5b1d772de89c784d44ea8e30399fdd
suzhijie <igeek_suzhijie@guoyanyunyan.local>
2018-01-03 21:39:41 +0800

    企业荣誉half

--- routers/about.js
+++ routers/about.js
@@ -6,7 +6,12 @@ router.get('/',async function (ctx,next) {
 router.get('/jtjs',async function (ctx,next) {
     await ctx.render('jtjs');
 });
-
+router.get('/hxjz',async function (ctx,next) {
+    await ctx.render('about_hxjz');
+});
+router.get('/qyry',async function (ctx,next) {
+    await ctx.render('about_qyry');
+});
 
 
 module.exports = router;
---
+++ src/es6/about/qyry.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// 进入页面时切换导航栏焦点,所有子页面都必须要有！！！这段代码
+var home = document.getElementsByClassName("nav_home")[0];
+var onPage = document.getElementsByClassName("nav_about")[0];
+var logo_bg = document.getElementById("log_bg");
+function navBarchange(event) {
+    var e = event || window.event;
+    home.classList.remove("active");
+    onPage.classList.add("active_about");
+    logo_bg.style.backgroundColor = "#f39e38";
+}
+window.addEventListener("load", navBarchange, false);
+
+// 分割线
+let openButton = document.getElementsByClassName('view_more')[0];
+let target = $('.block_view_more_1');
+let sign = true;
+openButton.addEventListener('click',function(){
+    target.toggleClass("showWord");
+    if(sign)
+    openButton.innerText = "收起";
+    else
+    openButton.innerText = "了解更多";
+    sign = !sign;
+},false);
+
+// 十七年文本位置
